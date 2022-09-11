e02e065c77f26db1620602a8e3094b25099a16be
Anshul Bansal <55446476+anshulbansal02@users.noreply.github.com>
2022-09-11 10:38:12 +0530

    revert last changes and added everything onload

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,20 @@
-gsap.registerPlugin(ScrollTrigger);
-
 $(window).on("load", () => {
+    gsap.registerPlugin(ScrollTrigger);
+
     // Header Timeline
     const headerTL = gsap.timeline();
     headerTL
-        // .to($("#logo-loader"), {
-        //     duration: 0.6,
-        //     rotate: "50deg",
-        //     yPercent: 200,
-        //     ease: "expo.in",
-        // })
-        // .to(
-        //     $("#preloader"),
-        //     { duration: 0.8, yPercent: 100, ease: "expo.in" },
-        //     "-=0.4"
-        // )
+        .to($("#logo-loader"), {
+            duration: 0.6,
+            rotate: "50deg",
+            yPercent: 200,
+            ease: "expo.in",
+        })
+        .to(
+            $("#preloader"),
+            { duration: 0.8, yPercent: 100, ease: "expo.in" },
+            "-=0.4"
+        )
         .from($(".header").find(".greeting"), {
             duration: 0.8,
             yPercent: 100,
@@ -35,10 +35,9 @@ $(window).on("load", () => {
             { duration: 1, autoAlpha: 0, yPercent: 10, ease: "expo.out" },
             "-=0.6"
         );
-});
 
-// Create Navigation
-$("nav").append(`
+    // Create Navigation
+    $("nav").append(`
 <ul>
 	<li class="to-projects"><a href="#">My Projects</a></li>
 	<li class="to-about"><a href="#">About Me</a></li>
@@ -57,103 +56,104 @@ $("nav").append(`
 </ul>
 `);
 
-// Wrap elements
-$(".wrapped").each((i, el) => {
-    $(el).removeClass("wrapped");
-    $(el).wrap("<div class='wrapped'></div>");
-});
+    // Wrap elements
+    $(".wrapped").each((i, el) => {
+        $(el).removeClass("wrapped");
+        $(el).wrap("<div class='wrapped'></div>");
+    });
 
-// Floating Navigation bar
-const navFloatAnimation = gsap.from(".bottom-nav", {
-    paused: true,
-    duration: 0.4,
-    ease: "power3.out",
-    y: 400,
-    scale: 0.9,
-});
-ScrollTrigger.create({
-    start: "100px top",
-    end: 99999,
+    // Floating Navigation bar
+    const navFloatAnimation = gsap.from(".bottom-nav", {
+        paused: true,
+        duration: 0.4,
+        ease: "power3.out",
+        y: 400,
+        scale: 0.9,
+    });
+    ScrollTrigger.create({
+        start: "100px top",
+        end: 99999,
 
-    onLeaveBack: () => {
-        navFloatAnimation.reverse();
-    },
-    onUpdate: (self) => {
-        self.direction === -1
-            ? navFloatAnimation.play()
-            : navFloatAnimation.reverse();
-    },
-});
+        onLeaveBack: () => {
+            navFloatAnimation.reverse();
+        },
+        onUpdate: (self) => {
+            self.direction === -1
+                ? navFloatAnimation.play()
+                : navFloatAnimation.reverse();
+        },
+    });
 
-// Navigation
+    // Navigation
 
-const controller = new ScrollMagic.Controller();
+    const controller = new ScrollMagic.Controller();
 
-new ScrollMagic.Scene({
-    triggerElement: "section.about",
-    duration: $("section.about").height(),
-})
-    .setClassToggle(".to-about", "active")
-    .addTo(controller);
-
-new ScrollMagic.Scene({
-    triggerElement: "section.projects",
-    duration: $("section.projects").height(),
-})
-    .setClassToggle(".to-projects", "active")
-    .addTo(controller);
-
-new ScrollMagic.Scene({
-    triggerElement: "section.footer",
-    duration: $("section.footer").height(),
-})
-    .setClassToggle(".to-contact", "active")
-    .addTo(controller);
-
-$(".to-projects").on("click", (e) => {
-    e.preventDefault();
-    $("section.projects")[0].scrollIntoView();
-});
-
-$(".to-about").on("click", (e) => {
-    e.preventDefault();
-    $("section.about")[0].scrollIntoView();
-});
-
-$(".to-contact").on("click", (e) => {
-    e.preventDefault();
-    $("section.footer")[0].scrollIntoView();
-});
-
-// Side Navigation
-
-const sideNavTL = gsap.timeline();
-
-sideNavTL
-    .from($(".side-nav").find("li"), {
-        y: -10,
-        autoAlpha: 0,
-        stagger: 0.04,
-        duration: 0.5,
-        ease: "back.out(0.8)",
+    new ScrollMagic.Scene({
+        triggerElement: "section.about",
+        duration: $("section.about").height(),
     })
-    .to(
-        $(".top-nav").find("li"),
-        {
+        .setClassToggle(".to-about", "active")
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+        triggerElement: "section.projects",
+        duration: $("section.projects").height(),
+    })
+        .setClassToggle(".to-projects", "active")
+        .addTo(controller);
+
+    new ScrollMagic.Scene({
+        triggerElement: "section.footer",
+        duration: $("section.footer").height(),
+    })
+        .setClassToggle(".to-contact", "active")
+        .addTo(controller);
+
+    $(".to-projects").on("click", (e) => {
+        e.preventDefault();
+        $("section.projects")[0].scrollIntoView();
+    });
+
+    $(".to-about").on("click", (e) => {
+        e.preventDefault();
+        $("section.about")[0].scrollIntoView();
+    });
+
+    $(".to-contact").on("click", (e) => {
+        e.preventDefault();
+        $("section.footer")[0].scrollIntoView();
+    });
+
+    // Side Navigation
+
+    const sideNavTL = gsap.timeline();
+
+    sideNavTL
+        .from($(".side-nav").find("li"), {
             y: -10,
+            autoAlpha: 0,
             stagger: 0.04,
             duration: 0.5,
-            autoAlpha: 0,
-            ease: "expo.inOut",
-        },
-        "-=0.75"
-    );
+            ease: "back.out(0.8)",
+        })
+        .to(
+            $(".top-nav").find("li"),
+            {
+                y: -10,
+                stagger: 0.04,
+                duration: 0.5,
+                autoAlpha: 0,
+                ease: "expo.inOut",
+            },
+            "-=0.75"
+        );
 
-new ScrollMagic.Scene({
-    triggerElement: ".header",
-    duration: 0,
-    offset: 20,
-    triggerHook: 0,
-})
-    .setTween(sideNavTL)
-    .addTo(controller);
+    new ScrollMagic.Scene({
+        triggerElement: ".header",
+        duration: 0,
+        offset: 20,
+        triggerHook: 0,
+    })
+        .setTween(sideNavTL)
+        .addTo(controller);
+});
