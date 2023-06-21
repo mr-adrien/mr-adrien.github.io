3f262c2e9ad74fb1ce50f450508de51398007fdc
Anshul Bansal <55446476+anshulbansal02@users.noreply.github.com>
2023-06-21 21:31:53 +0530

    Formatting changes and resume updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,43 @@
 $(window).on("load", () => {
-    gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollTrigger);
 
-    // Header Timeline
-    const headerTL = gsap.timeline();
-    headerTL
-        .to($("#logo-loader"), {
-            duration: 0.6,
-            rotate: "50deg",
-            yPercent: 200,
-            ease: "expo.in",
-        })
-        .to(
-            $("#preloader"),
-            { duration: 0.8, yPercent: 100, ease: "expo.in" },
-            "-=0.4"
-        )
-        .from($(".header").find(".greeting"), {
-            duration: 0.8,
-            yPercent: 100,
-            ease: "expo.out",
-        })
-        .from(
-            $(".header").find(".name"),
-            { duration: 0.8, yPercent: 120, ease: "expo.out" },
-            "-=0.6"
-        )
-        .from(
-            $(".header").find(".intro"),
-            { duration: 0.8, autoAlpha: 0, yPercent: 60, ease: "expo.out" },
-            "-=0.4"
-        )
-        .from(
-            $(".top-nav"),
-            { duration: 1, autoAlpha: 0, yPercent: 10, ease: "expo.out" },
-            "-=0.6"
-        );
+  // Header Timeline
+  const headerTL = gsap.timeline();
+  headerTL
+    .to($("#logo-loader"), {
+      duration: 0.6,
+      rotate: "50deg",
+      yPercent: 200,
+      ease: "expo.in",
+    })
+    .to(
+      $("#preloader"),
+      { duration: 0.8, yPercent: 100, ease: "expo.in" },
+      "-=0.4"
+    )
+    .from($(".header").find(".greeting"), {
+      duration: 0.8,
+      yPercent: 100,
+      ease: "expo.out",
+    })
+    .from(
+      $(".header").find(".name"),
+      { duration: 0.8, yPercent: 120, ease: "expo.out" },
+      "-=0.6"
+    )
+    .from(
+      $(".header").find(".intro"),
+      { duration: 0.8, autoAlpha: 0, yPercent: 60, ease: "expo.out" },
+      "-=0.4"
+    )
+    .from(
+      $(".top-nav"),
+      { duration: 1, autoAlpha: 0, yPercent: 10, ease: "expo.out" },
+      "-=0.6"
+    );
 
-    // Create Navigation
-    $("nav").append(`
+  // Create Navigation
+  $("nav").append(`
 <ul>
 	<li class="to-projects"><a href="#">My Projects</a></li>
 	<li class="to-about"><a href="#">About Me</a></li>
@@ -45,7 +45,7 @@ $(window).on("load", () => {
 	<li>
 		<button
 			title="Download Resume"
-			onclick="location.href='https://drive.google.com/uc?export=download&id=1EYnwdPMQR36tRt4MlK06fzoQU8c9C8tG';"
+			onclick="window.open('./files/Anshul_Bansal_Resume.pdf');"
 		>
 			<img
 				src="./img/resume.svg"
@@ -57,104 +57,104 @@ $(window).on("load", () => {
 </ul>
 `);
 
-    // Wrap elements
-    $(".wrapped").each((i, el) => {
-        $(el).removeClass("wrapped");
-        $(el).wrap("<div class='wrapped'></div>");
-    });
+  // Wrap elements
+  $(".wrapped").each((i, el) => {
+    $(el).removeClass("wrapped");
+    $(el).wrap("<div class='wrapped'></div>");
+  });
 
-    // Floating Navigation bar
-    const navFloatAnimation = gsap.from(".bottom-nav", {
-        paused: true,
-        duration: 0.4,
-        ease: "power3.out",
-        y: 400,
-        scale: 0.9,
-    });
-    ScrollTrigger.create({
-        start: "100px top",
-        end: 99999,
+  // Floating Navigation bar
+  const navFloatAnimation = gsap.from(".bottom-nav", {
+    paused: true,
+    duration: 0.4,
+    ease: "power3.out",
+    y: 400,
+    scale: 0.9,
+  });
+  ScrollTrigger.create({
+    start: "100px top",
+    end: 99999,
 
-        onLeaveBack: () => {
-            navFloatAnimation.reverse();
-        },
-        onUpdate: (self) => {
-            self.direction === -1
-                ? navFloatAnimation.play()
-                : navFloatAnimation.reverse();
-        },
-    });
+    onLeaveBack: () => {
+      navFloatAnimation.reverse();
+    },
+    onUpdate: (self) => {
+      self.direction === -1
+        ? navFloatAnimation.play()
+        : navFloatAnimation.reverse();
+    },
+  });
 
-    // Navigation
+  // Navigation
 
-    const controller = new ScrollMagic.Controller();
+  const controller = new ScrollMagic.Controller();
 
-    new ScrollMagic.Scene({
-        triggerElement: "section.about",
-        duration: $("section.about").height(),
+  new ScrollMagic.Scene({
+    triggerElement: "section.about",
+    duration: $("section.about").height(),
+  })
+    .setClassToggle(".to-about", "active")
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerElement: "section.projects",
+    duration: $("section.projects").height(),
+  })
+    .setClassToggle(".to-projects", "active")
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerElement: "section.footer",
+    duration: $("section.footer").height(),
+  })
+    .setClassToggle(".to-contact", "active")
+    .addTo(controller);
+
+  $(".to-projects").on("click", (e) => {
+    e.preventDefault();
+    $("section.projects")[0].scrollIntoView();
+  });
+
+  $(".to-about").on("click", (e) => {
+    e.preventDefault();
+    $("section.about")[0].scrollIntoView();
+  });
+
+  $(".to-contact").on("click", (e) => {
+    e.preventDefault();
+    $("section.footer")[0].scrollIntoView();
+  });
+
+  // Side Navigation
+
+  const sideNavTL = gsap.timeline();
+
+  sideNavTL
+    .from($(".side-nav").find("li"), {
+      y: -10,
+      autoAlpha: 0,
+      stagger: 0.04,
+      duration: 0.5,
+      ease: "back.out(0.8)",
     })
-        .setClassToggle(".to-about", "active")
-        .addTo(controller);
+    .to(
+      $(".top-nav").find("li"),
+      {
+        y: -10,
+        stagger: 0.04,
+        duration: 0.5,
+        autoAlpha: 0,
+        ease: "expo.inOut",
+      },
+      "-=0.75"
+    );
 
-    new ScrollMagic.Scene({
-        triggerElement: "section.projects",
-        duration: $("section.projects").height(),
-    })
-        .setClassToggle(".to-projects", "active")
-        .addTo(controller);
-
-    new ScrollMagic.Scene({
-        triggerElement: "section.footer",
-        duration: $("section.footer").height(),
-    })
-        .setClassToggle(".to-contact", "active")
-        .addTo(controller);
-
-    $(".to-projects").on("click", (e) => {
-        e.preventDefault();
-        $("section.projects")[0].scrollIntoView();
-    });
-
-    $(".to-about").on("click", (e) => {
-        e.preventDefault();
-        $("section.about")[0].scrollIntoView();
-    });
-
-    $(".to-contact").on("click", (e) => {
-        e.preventDefault();
-        $("section.footer")[0].scrollIntoView();
-    });
-
-    // Side Navigation
-
-    const sideNavTL = gsap.timeline();
-
-    sideNavTL
-        .from($(".side-nav").find("li"), {
-            y: -10,
-            autoAlpha: 0,
-            stagger: 0.04,
-            duration: 0.5,
-            ease: "back.out(0.8)",
-        })
-        .to(
-            $(".top-nav").find("li"),
-            {
-                y: -10,
-                stagger: 0.04,
-                duration: 0.5,
-                autoAlpha: 0,
-                ease: "expo.inOut",
-            },
-            "-=0.75"
-        );
-
-    new ScrollMagic.Scene({
-        triggerElement: ".header",
-        duration: 0,
-        offset: 20,
-        triggerHook: 0,
-    })
-        .setTween(sideNavTL)
-        .addTo(controller);
+  new ScrollMagic.Scene({
+    triggerElement: ".header",
+    duration: 0,
+    offset: 20,
+    triggerHook: 0,
+  })
+    .setTween(sideNavTL)
+    .addTo(controller);
 });
